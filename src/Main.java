b8073348f72f3b0e366acc31c0dedebd855a71be
AndreevDan93 <andreevdan93@gmail.com>
2022-06-20 14:11:24 +0500

    addedKeyListener

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,32 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.IOException;
 
-public class Main extends JFrame {
+public class Main extends JFrame implements KeyListener {
     static Main window = new Main();
+    static int backgroundX = 0;
+    static int backgroundY = 0;
     static int weight = 800;
     static int height = 600;
     static int imageSize = 50;
     static Image background;
+
+    public Main (){
+        addKeyListener(this);
+    }
 
     public static void main(String[] args) throws IOException {
         initFrame();
         background = ImageIO.read(Main.class.getResourceAsStream("background.jpg"));
         Painting painting = new Painting();
         window.add(painting);
+
         window.setVisible(true);
+
 
 
     }
@@ -29,16 +39,46 @@ public class Main extends JFrame {
 
     }
 
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case  KeyEvent.VK_A:
+                backgroundX = backgroundX - imageSize;
+                break;
+            case KeyEvent.VK_D:
+                backgroundX = backgroundX + imageSize;
+                break;
+            case KeyEvent.VK_W:
+                backgroundY = backgroundY - imageSize;
+                break;
+            case KeyEvent.VK_S:
+                backgroundY = backgroundY + imageSize;
+                break;
+
+
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
     private static class Painting extends JPanel {
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
-            for (int x = 0; x <= weight - imageSize; x += imageSize) {
-                for (int y = 0; y <= height - imageSize; y += imageSize) {
-                    g.drawImage(background, x, y, null);
 
-                }
-            }
+            g.drawImage(background, backgroundX, backgroundY, null);
+            repaint();
+
+
 
         }
     }
